bf7ffb6802d60b5ea2696fba1893521f987778e6
ntthau <ntthau0963@gmail.com>
2025-01-08 14:54:27 +0700

    init friend, lobby, authen, cloudsave

--- Assets/Scripts/Managers/SceneTransition.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using UnityEngine;
- 
-public abstract class SceneTransition : MonoBehaviour
-{
-    public abstract IEnumerator AnimateTransitionIn();
-    public abstract IEnumerator AnimateTransitionOut();
-}
